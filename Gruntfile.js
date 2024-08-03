1de7964191807f1beb2d422c275008ff694c3bee
Fredaobob <lokgamer01@gmail.com>
2024-08-03 13:57:22 -0300

    task finished

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,109 +1,48 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
       less: {
-        development: {
-          files: {
-            'dev/styles/main.css': 'src/styles/main.less'
+          development: {
+              files: {
+                  'dev/styles/main.css': 'src/styles/main.less'
+              }
           }
-        },
-        production: {
-          options: {
-            compress: true,
-          },
-          files: {
-            'dist/styles/main.min.css': 'src/styles/main.less'
-          }
-        }
       },
-      watch: {
-        less: {
-          files: ['src/styles/**/*.less'],
-          tasks: ['less:development'] 
-        },
-        html: {
-          files: ['src/index.html'],
-          tasks: ['replace:dev']
-        }
+      uglify: {
+          build: {
+              files: {
+                  'dev/scripts/script.min.js': ['src/scripts/script.js']
+              }
+          }
       },
       replace: {
-        dev: {
-          options: {
-            patterns: [ 
-              {
-                match: 'ENDERECO_DO_CSS',
-                replacement: './styles/main.css'
+          updatePaths: {
+              options: {
+                  patterns: [
+                      {
+                          match: 'ENDERECO_DO_CSS',
+                          replacement: './styles/main.css'
+                      },
+                      {
+                          match: 'ENDERECO_DO_JS',
+                          replacement: './scripts/script.min.js'
+                      }
+                  ]
               },
-              
-              {
-                match: 'ENDERECO_DO_JS',
-                replacement: '../src/scripts/main.js'
-              },
-              
-            ]
-          },
-          files: [
-            {
-              expand: true,
-              flatten: true,
-              src: ['src/index.html'],
-              dest: 'dev/'
-            }
-          ]
-        },
-        dist: {
-          options: {
-            patterns: [ 
-              {
-                match: 'ENDERECO_DO_CSS',
-                replacement: './styles/main.min.css'
-              },
-              {
-                match: 'ENDERECO_DO_JS',
-                replacement: './scripts/main.min.js'
-              },
-            ]
-          },
-          files: [
-            {
-              expand: true,
-              flatten: true,
-              src: ['prebuild/index.html'],
-              dest: 'dist/'
-            }
-          ]
-        }
-      },
-      htmlmin: {
-        dist: {
-          options: {
-            removeComments: true,
-            collapseWhitespace: true
-          },
-          files: {
-            'prebuild/index.html': 'src/index.html'
+              files: [
+                  {
+                      expand: true,
+                      flatten: true,
+                      src: ['src/index.html'],
+                      dest: 'dev/'
+                  }
+              ]
           }
-        }
-      },
-      clean: ['prebuild'],
-      uglify: {
-        target: {
-          files: {
-            'dist/scripts/main.min.js': 'src/scripts/main.js'
-          }
-        }
       }
-      
-    })
+  })
 
+  grunt.loadNpmTasks('grunt-contrib-less')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-replace')
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+  grunt.registerTask('default', ['less', 'uglify', 'replace:updatePaths'])
 }
